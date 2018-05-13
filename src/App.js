05da9881bc8d60654ec30b8c631123587cd7e5ce
Bruker <magnus01lysfjord@gmail.com>
2018-05-13 11:28:05 +0200

    searchUrl Props

--- src/App.js
+++ src/App.js
@@ -4,54 +4,64 @@ import Navigation from './comp/Header/Navigation';
 import Profile from './comp/Header/Profile';
 import MovieOfTheDay from './comp/TopSection/MovieOfTheDay';
 import ShowSlider from './comp/BottomSection/SimpleMapped';
+import { connect } from "react-redux";
+
+import {searchUrl } from '../actions/movies';
+
 import './App.css';
-// debounce   , Library => lodash
+
+
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      searchTerm:"",
-      searchUrl:""
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchTerm:"",
+            searchUrl:""
+        }
+
+        this.handleChange.bind(this)
+        this.handleKeyUp.bind(this)
     }
 
-    this.handleChange.bind(this)
-    this.handleKeyUp.bind(this)
-  }
+    handleKeyUp = (event) => {
+        if(this.state.searchTerm !== "") //event.key === 'Enter' &&
+        {
+            var searchUrl =  "search/multi?query=" + this.state.searchTerm + "&api_key=166624c030b91c943c397020f20525b4";
+            this.props.searchUrl();
 
-  handleKeyUp = (event) => {
-    if(this.state.searchTerm !== "") //event.key === 'Enter' && 
-    {
-      var searchUrl =  "search/multi?query=" + this.state.searchTerm + "&api_key=166624c030b91c943c397020f20525b4";
-      this.setState({
-        searchUrl: searchUrl
-      })
+        }
     }
-  }
 
-  handleChange = (event) => {
-    this.setState({
-      searchTerm: event.target.value
-    })
-  }
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
 
-  render(){
-    return (
-      <div>
-        <header className="Header">
-          <Logo />
-          <Navigation />
-          <div id="search" className="Search">
-            <input onChange={this.handleChange} onKeyUp={this.handleKeyUp} value={this.state.searchTerm} placeholder="Search for a title..."/>
-          </div>
-
-          <Profile />
-        </header>
-
-
-        <ShowSlider title="Search Results" url={this.state.searchUrl}/>
-
-      </div>
-    )
-  }
+    render(){
+        return (
+            <div>
+              <header className="Header">
+                <Logo />
+                <Navigation />
+                <div id="search" className="Search">
+                  <input onChange={this.handleChange} onKeyUp={this.handleKeyUp} value={this.state.searchTerm} placeholder="Search for a title..."/>
+                </div>
+                <Profile />
+              </header>
+              <MovieOfTheDay />
+              <ShowSlider title="Search Results" url={this.props.movies.searchUrl}/>
+            </div>
+        )
+    }
 }
-export default App;
+
+function mapStateToProps(state) {
+    console.log();
+    return {
+        movies: state.movies
+    }
+}
+
+
+export default connect(mapStateToProps, {searchUrl})(App);
